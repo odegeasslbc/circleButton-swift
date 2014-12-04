eb14f9e4e0721925552d597ab49c55ce31edae12
刘炳辰 <534060553@qq.com>
2014-12-04 22:15:30 +0800

    Initial Commit

---
+++ circleButton/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  circleButton
+//
+//  Created by 刘炳辰 on 14/12/4.
+//  Copyright (c) 2014年 刘炳辰. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
